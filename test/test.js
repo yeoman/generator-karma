e72207b160581e8ad1c02f19cde54caa3e46d314
Kevin Martensson <kevinmartensson@gmail.com>
2013-04-15 15:35:20 +0200

    Refactor
    * Use `test-framework` option
    * Added tests
    * Code style
    
    Fixes #8 and #16.

--- test/test.js
+++ test/test.js
@@ -1,31 +1,33 @@
-/*global describe before it */
+/*global describe, beforeEach, it */
 'use strict';
+
 var path = require('path');
-var assert  = require('assert');
 var helpers = require('yeoman-generator').test;
 
-
 describe('Karma generator test', function () {
-  before(helpers.before(path.join(__dirname, './temp')));
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
 
- /* it('every generator can be required without throwing', function () {
-    // not testing the actual run of generators yet
-    this.app = require('../app');
+      this.app = helpers.createGenerator('karma:app', [
+        '../../lib/generators/app'
+      ]);
+      done();
+    }.bind(this));
   });
 
-  // FIXME
-  it.skip('creates expected files', function () {
-    // Use helpers.assertFile() to help you test the output of your generator
-    //
-    // Example:
-    //
-    //    // check file exists
-    //    helpers.assertFile('app/model/post.js');
-    //    // Check content
-    //    helpers.assertFile('app/model/post.js', /Backbone\.model/);
-    it('should create expected files');
-  });*/
-  it('fixme', function() {
-    assert(true);
+  it('creates expected files', function (done) {
+    var expected = [
+      'karma.conf.js',
+      'karma-e2e.conf.js'
+    ];
+
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
   });
 });
